dd6c401e4fe7c407b4f2bf360347cd9621e88714
Anjali0739 <anjalisoni0739@gmail.com>
2025-10-21 15:32:07 +0530

    prime number

---
+++ logic_build_up/Prime_Number.py
@@ -0,0 +1,26 @@
+# Question: https://www.geeksforgeeks.org/problems/prime-number2314/1
+
+class Solution:
+    def isPrime(self, n):
+        # code here
+
+        
+        count = 0
+        
+        # method 1:
+        # for i in range(1, n+1):
+        #     if n%i==0:
+        #         count+=1
+            
+        # if count>2 or n<=1:
+        #     return False
+        # return True
+        
+        
+        # method 2:
+        for i in range(1, int(math.sqrt(n))+1):
+            if n%i==0 or n%(i*i)==0:
+                count+=1
+        if count>=2 or n<=1:
+            return False
+        return True
